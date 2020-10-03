e8dac41d38f24772f61782ac0f04a1be0795382f
Jeff Innes <contact.me@jeffinnes.dev>
2020-10-02 22:58:50 -0700

    Very basic structure in place.
    Clock seems to display correct time and color.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <clock />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import clock from './components/clock.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    clock,
   }
 }
 </script>
---
+++ src/components/clock.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :style="{ backgroundColor: hexTime }">
+    <h1>{{ hexTime }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'clock',
+  data () {
+    return {
+      hexTime: '#000000'
+    }
+  },
+  mounted () {
+    setInterval(() => {
+      let now = new Date(Date.now());
+      this.hexTime = `#${now.getHours()}${now.getMinutes()}${now.getSeconds()}`
+    }, 1000);
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
