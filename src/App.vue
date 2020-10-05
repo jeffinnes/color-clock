09c3019c0939b3c61a3c1b166eacbbcff0e5d0b1
Jeff Innes <contact.me@jeffinnes.dev>
2020-10-04 17:34:47 -0700

    Adds user input section

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <clock />
+  <userInput />
 </template>
 
 <script>
-import clock from './components/clock.vue'
+import clock from './components/clock.vue';
+import userInput from './components/userInput';
 
 export default {
   name: 'App',
   components: {
     clock,
+    userInput,
   }
 }
 </script>
 
 <style>
+html, body {
+  height: 100%;
+  margin: 0;
+  font-size: 10px;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "GeosansLight-NMS", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: 2fr 1fr;
+  height: 100%;
 }
 </style>
--- src/components/userInput.vue
+++ src/components/userInput.vue
@@ -1,13 +1,57 @@
 <template>
-  
+<div id="userInput">
+  <p>The current base color code is {{ baseColor }}. You can set a different base color and see how it changes the day's colors below.</p>
+  <div class="color-selections">
+    <div class="base-color-selector" :style="{backgroundColor: base1}" @click="updatePreview(base1)">{{ base1 }}</div>
+    <div class="base-color-selector" :style="{backgroundColor: base2}" @click="updatePreview(base2)">{{ base2 }}</div>
+    <div class="base-color-selector" :style="{backgroundColor: base3}" @click="updatePreview(base3)">{{ base3 }}</div>
+    <div class="base-color-selector" :style="{backgroundColor: base4}" @click="updatePreview(base4)">{{ base4 }}</div>
+    <div class="base-color-selector" :style="{backgroundColor: base5, color: '#fff'}" @click="updatePreview(base5)">{{ base5 }}</div>
+    <div class="base-color-selector" :style="{backgroundColor: base6, color: '#fff'}" @click="updatePreview(base6)">{{ base6 }}</div>
+  </div>
+  <div id="colorPreview">The color preview will go here</div>
+</div>
 </template>
 
 <script>
 export default {
   name: 'userInput',
+  data () {
+    return {
+      baseColor: "#000000",
+      base1: "#bb0000",
+      base2: "#bb00bb",
+      base3: "#00bb00",
+      base4: "#00bbbb",
+      base5: "#0000bb",
+      base6: "#000000",
+    }
+  },
+  methods: {
+    updatePreview(newBaseColor) {
+      this.baseColor = newBaseColor;
+    },
+  }
 }
 </script>
 
-<style>
+<style scoped>
+#userInput {
+  grid-column: span 12;
+  font-size: 2.5rem;
+}
+
+div.color-selections {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+
+div.base-color-selector {
+  padding: .5rem 1rem;
+  margin: .5rem;
+  border: 1px solid #000000;
+  font-weight: bold;
+}
 
 </style>
