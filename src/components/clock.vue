f3542193dd557bfe81a07e2b0deace49e926fd97
Jeff Innes <contact.me@jeffinnes.dev>
2020-10-08 15:54:25 -0700

    Check clock background to text color ratio and adjusts to maintain readability

--- src/components/clock.vue
+++ src/components/clock.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="clock" :style="{ backgroundColor: hexColor }">
+  <div id="clock" :style="{ backgroundColor: hexColor, color: textColor }">
     <span>{{ hexTime }}</span>
   </div>
 </template>
@@ -12,6 +12,8 @@ export default {
     return {
       hexTime: '',
       hexColor: '',
+      textColor: '',
+      originalTextColor: '#eaf6e5',
       baseHour: "00",
       baseMinute: "00",
       baseSecond: "00",
@@ -55,6 +57,7 @@ export default {
       
       this.hexTime = `#${hour}${minute}${second}`;
       this.hexColor = `#${shiftedHour}${shiftedMinute}${shiftedSecond}`;
+      this.textColor = this.setTextColor(shiftedHour, shiftedMinute, shiftedSecond);
 
       /* return {
         displayTime: `#${hour}${minute}${second}`,
@@ -69,6 +72,44 @@ export default {
       }
 
       return Number(intSum).toString(16);
+    },
+    setTextColor(r, g, b) {
+      const lightFontLuminance = 0.8906110235423622;
+      /** Guidance for the luminance logic comes from this article by Alvaro Montoro
+       * https://dev.to/alvaromontoro/building-your-own-color-contrast-checker-4j7o
+       * 
+       * Relative luminance of a color is defined as L = 0.2126 * R + 0.7152 * G + 0.0722 * B
+       * where R, G and B are defined as:
+       * if RsRGB <= 0.03928 then R = RsRGB/12.92 else R = ((RsRGB+0.055)/1.055) ^ 2.4
+       * if GsRGB <= 0.03928 then G = GsRGB/12.92 else G = ((GsRGB+0.055)/1.055) ^ 2.4
+       * if BsRGB <= 0.03928 then B = BsRGB/12.92 else B = ((BsRGB+0.055)/1.055) ^ 2.4
+       * 
+       * RsRGB, GsRGB, and BsRGB are defined as:
+       * RsRGB = R8bit/255
+       * GsRGB = G8bit/255
+       * BsRGB = B8bit/255
+       */
+      const processedRGBArray = [r, g, b].map((val) => {
+        let intVal = parseInt(val, 16);
+        intVal = intVal / 255;
+        if (intVal <= 0.03928) {
+          // Return this value to be stored inthe new array
+          return intVal / 12.92;
+        } else {
+          // Return this value to be stored inthe new array
+          return Math.pow((intVal + 0.055) / 1.055, 2.4);
+        }
+      });
+
+      const luminanceOfCurTime = (0.2126 * processedRGBArray[0]) + (0.7152 * processedRGBArray[1]) + (0.0722 * processedRGBArray[2]);
+
+      const contrastRatio = (luminanceOfCurTime + 0.05) / (lightFontLuminance + 0.05);
+      console.log(`contrastRatio: ${contrastRatio}`);
+      if (contrastRatio > 0.22222) {
+        return '#000000';
+      } else {
+        return this.originalTextColor;
+      }
     },
   },
   beforeMount() {
@@ -97,10 +138,11 @@ export default {
 <style scoped>
 #clock {
   font-size: 10rem;
-  color: #eaf6e5;
+  font-weight: 800;
   grid-column: span 12;
   display: flex;
   justify-content: center;
   align-items: center;
+  padding: 0 1.5rem;
 }
 </style>
