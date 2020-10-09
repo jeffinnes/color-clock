7210f7363ee79162d9370d130d411efe75d250fc
Jeff Innes <contact.me@jeffinnes.dev>
2020-10-08 21:02:12 -0700

    Change font color of buttons to be more readable

--- src/components/userInput.vue
+++ src/components/userInput.vue
@@ -3,12 +3,12 @@
   <p>The current base color code is {{ baseColor }}.</p>
   <p>You can set a different base color below and see how it changes the current time's color.</p>
   <div class="color-selections">
-    <div class="base-color-selector" :style="{backgroundColor: base1}" @click="updatePreview(base1)">{{ base1 }}</div>
-    <div class="base-color-selector" :style="{backgroundColor: base2}" @click="updatePreview(base2)">{{ base2 }}</div>
+    <div class="base-color-selector" :style="{backgroundColor: base1, color: '#ffffff'}" @click="updatePreview(base1)">{{ base1 }}</div>
+    <div class="base-color-selector" :style="{backgroundColor: base2, color: '#ffffff'}" @click="updatePreview(base2)">{{ base2 }}</div>
     <div class="base-color-selector" :style="{backgroundColor: base3}" @click="updatePreview(base3)">{{ base3 }}</div>
     <div class="base-color-selector" :style="{backgroundColor: base4}" @click="updatePreview(base4)">{{ base4 }}</div>
-    <div class="base-color-selector" :style="{backgroundColor: base5, color: '#fff'}" @click="updatePreview(base5)">{{ base5 }}</div>
-    <div class="base-color-selector" :style="{backgroundColor: base6, color: '#fff'}" @click="updatePreview(base6)">{{ base6 }}</div>
+    <div class="base-color-selector" :style="{backgroundColor: base5, color: '#ffffff'}" @click="updatePreview(base5)">{{ base5 }}</div>
+    <div class="base-color-selector" :style="{backgroundColor: base6, color: '#ffffff'}" @click="updatePreview(base6)">{{ base6 }}</div>
   </div>
 </div>
 </template>
